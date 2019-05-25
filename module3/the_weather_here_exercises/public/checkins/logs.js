b5a38b0ce806d510b4596be66fa6092e2076bc54
shiffman <daniel@shiffman.net>
2019-05-25 14:15:25 -0400

    the rest of the exercises for weather here #38

--- module3/the_weather_here_exercises/public/checkins/logs.js
+++ module3/the_weather_here_exercises/public/checkins/logs.js
@@ -2,8 +2,7 @@ const mymap = L.map('checkinMap').setView([0, 0], 1);
 const attribution =
   '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 //const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-const tileUrl =
-  'https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png';
+const tileUrl = 'https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png';
 const tiles = L.tileLayer(tileUrl, { attribution });
 tiles.addTo(mymap);
 
@@ -12,21 +11,34 @@ getData();
 async function getData() {
   const response = await fetch('/api');
   const data = await response.json();
+  const pointList = [];
 
   for (item of data) {
+    pointList.push([item.lat, item.lon]);
+    console.log(item);
     const marker = L.marker([item.lat, item.lon]).addTo(mymap);
-    let txt = `The weather here at ${item.lat}&deg;,
-    ${item.lon}&deg; is ${item.weather.summary} with
-    a temperature of ${item.weather.temperature}&deg; C.`;
-
+    let txt = `I'm sitting out here at ${item.lat}&deg;,  ${item.lon}&deg;, on
+    this ${item.weather.summary} day and it feels like ${item.weather.temperature}&deg; outside.`;
     if (item.air.value < 0) {
       txt += '  No air quality reading.';
     } else {
-      txt += `  The concentration of particulate matter 
-    (${item.air.parameter}) is ${item.air.value} 
-    ${item.air.unit} last read on ${item.air.lastUpdated}`;
+      txt += `  
+      The concentration of small carcinogenic particles (${item.air.measurements[0].parameter}) I'm
+        breathing in is  ${item.air.measurements[0].value} ${
+        item.air.measurements[0].unit
+      } measured from
+       ${item.air.city} at ${item.air.location} on ${item.air.measurements[0].lastUpdated}.`;
     }
     marker.bindPopup(txt);
   }
-  console.log(data);
+
+  // Add a line path
+
+  const polyLine = new L.Polyline(pointList, {
+    color: 'purple',
+    weight: 3,
+    opacity: 1,
+    smoothFactor: 1
+  });
+  polyLine.addTo(mymap);
 }
